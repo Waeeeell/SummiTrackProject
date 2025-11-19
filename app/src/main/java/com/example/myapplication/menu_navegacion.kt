797805450b9f036786eee293642d051b0e19be0c
Waeeeell <waelelk053@gmail.com>
2025-11-19 19:56:25 +0100

    empezando menu principal, problemas... de momento la app peta al pasar de iniciar sesion a menu navegación

--- app/src/main/java/com/example/myapplication/menu_navegacion.kt
+++ app/src/main/java/com/example/myapplication/menu_navegacion.kt
@@ -1,15 +1,33 @@
 package com.example.myapplication
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class menu_navegacion : AppCompatActivity() {
+
+    private lateinit var logo: ImageView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_menu_navegacion)
+
+        initComponent()
+        initLiteners()
+    }
+    private fun initComponent(){
+        logo = findViewById<ImageView>(R.id.logo)
+    }
+
+    private fun initLiteners(){
+        logo.setOnClickListener {
+            val intent = Intent(this, Menu_perfil::class.java)
+            startActivity(intent)
+        }
     }
 }
